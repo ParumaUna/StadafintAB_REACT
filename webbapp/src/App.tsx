695b14791121544a4fe29260cbaf7a4246812f6d
Una Paruma <parumauna@gmail.com>
2023-03-22 21:36:59 +0100

    main page design

--- webbapp/src/App.tsx
+++ webbapp/src/App.tsx
@@ -1,4 +1,5 @@
 import Bootstrap from "./components/bootstrap";
+import './App.css'
 
 function App(){
   return(
--- webbapp/src/components/bootstrap.tsx
+++ webbapp/src/components/bootstrap.tsx
@@ -4,178 +4,118 @@ function Bootstrap() {
     return (
       <>
         <header>
-        <div className="collapse bg-dark" id="navbarHeader">
-            <div className="container">
-                <div className="row">
-                <div className="col-sm-8 col-md-7 py-4">
-                <h4 className="text-white">About</h4>
-                <p className="text-muted">Add some information about the album below, the author, or any other background context. Make it a few sentences long so folks can pick up some informative tidbits. Then, link them off to some social networking sites or contact information.</p>
-              </div>
-              <div className="col-sm-4 offset-md-1 py-4">
-                <h4 className="text-white">Contact</h4>
-                <ul className="list-unstyled">
-                  <li><a href="#" className="text-white">Follow on Twitter</a></li>
-                  <li><a href="#" className="text-white">Like on Facebook</a></li>
-                  <li><a href="#" className="text-white">Email me</a></li>
-                </ul>
-              </div>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
+          <div className="navbar navbar-dark box-shadow">
+            <div className="container d-flex">
+              <h1 className="Logo">StädaFint</h1>
+              <a href="#" className="navbar-brand d-flex align-items-center">
+              <i className="fa fa-user" ></i>
+                <a className="login-link">
+                  <strong className="Login">Logga-in</strong>
+                </a>
+              </a>
             </div>
           </div>
-        </div>
-        <div className="navbar navbar-dark bg-dark box-shadow">
-          <div className="container d-flex justify-content-between">
-            <a href="#" className="navbar-brand d-flex align-items-center">
-              <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="mr-2"><path d="M23 19a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h4l2-3h6l2 3h4a2 2 0 0 1 2 2z"></path><circle cx="12" cy="13" r="4"></circle></svg>
-              <strong>Album</strong>
-            </a>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
-              <span className="navbar-toggler-icon"></span>
-            </button>
-          </div>
-        </div>
-      </header>
+        </header>
   
-      <main role="main">
+        <main className="main">
   
-        <section className="jumbotron text-center">
-          <div className="container">
-            <h1 className="jumbotron-heading">Städfirma</h1>
-            <p className="lead text-muted">Something short and leading about the collection below—its contents, the creator, etc. Make it short and sweet, but not too short so folks don't simply skip over it entirely.</p>
-            <p>
-              <a href="#" className="btn btn-primary my-2">Main call to action</a>
-              <a href="#" className="btn btn-secondary my-2">Secondary action</a>
-            </p>
-          </div>
-        </section>
-  
-        <div className="album py-5 bg-light">
-          <div className="container">
-  
+          <section className="jumbotron text-center">
+            <div className="container">
+              <img className="card-img-top" data-src="loundry-pic.jpeg" src="loundry-pic.jpeg" data-holder-rendered="true" ></img>
+              <div className="jumbotron-heading">
+              </div>
+            </div>
+          </section>
+
+          <div className="album py-5 bg-light">
+            <div className="container">
+              <div className="äranden">
+                <h1>Vad kan vi hjälpa dig med?</h1>
+                <hr />
+              </div>
             <div className="row">
               <div className="col-md-4">
                 <div className="card mb-4 box-shadow">
-                <img className="card-img-top" data-src="nedladdning.jpeg" alt="Thumbnail [100%x225]" src="nedladdning.jpeg" data-holder-rendered="true"></img>
-                  <div className="card-body">
-                    <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                <img className="card-img-top" data-src="helhets-städning.jpeg" alt="Thumbnail [100%x225]" src="helhets-städning.jpeg" data-holder-rendered="true"></img>
+                <div className="card-body">
+                    <p className="card-text">Allmänt/ alla utrymmen </p>
                     <div className="d-flex justify-content-between align-items-center">
                       <div className="btn-group">
-                        <button type="button" className="btn btn-sm btn-outline-secondary">View</button>
-                        <button type="button" className="btn btn-sm btn-outline-secondary">Edit</button>
+                        <button type="button" className="btn btn-sm btn-outline-secondary">Boka</button>
                       </div>
-                      <small className="text-muted">9 mins</small>
+                      <small className="text-muted">ca. 2h</small>
                     </div>
                   </div>
                 </div>
               </div>
               <div className="col-md-4">
                 <div className="card mb-4 box-shadow">
+                <img className="card-img-top" data-src="vardagsrummet.jpeg" alt="Thumbnail [100%x225]" src="vardagsrummet.jpeg" data-holder-rendered="true"></img>
                   <div className="card-body">
-                    <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <p className="card-text">Vardagsrummet</p>
                     <div className="d-flex justify-content-between align-items-center">
                       <div className="btn-group">
-                        <button type="button" className="btn btn-sm btn-outline-secondary">View</button>
-                        <button type="button" className="btn btn-sm btn-outline-secondary">Edit</button>
+                        <button type="button" className="btn btn-sm btn-outline-secondary">Boka</button>
                       </div>
-                      <small className="text-muted">9 mins</small>
+                      <small className="text-muted">Ca. 1h</small>
                     </div>
                   </div>
                 </div>
               </div>
               <div className="col-md-4">
                 <div className="card mb-4 box-shadow">
+                <img className="card-img-top" data-src="nedladdning.jpeg" alt="[100%x225]" src="nedladdning.jpeg" data-holder-rendered="true"></img>
                   <div className="card-body">
-                    <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <p className="card-text">Köket</p>
                     <div className="d-flex justify-content-between align-items-center">
                       <div className="btn-group">
-                        <button type="button" className="btn btn-sm btn-outline-secondary">View</button>
-                        <button type="button" className="btn btn-sm btn-outline-secondary">Edit</button>
+                        <button type="button" className="btn btn-sm btn-outline-secondary">Boka</button>
                       </div>
-                      <small className="text-muted">9 mins</small>
+                      <small className="text-muted">Ca. 2h</small>
                     </div>
                   </div>
                 </div>
               </div>
-  
+
               <div className="col-md-4">
                 <div className="card mb-4 box-shadow">
+                <img className="card-img-top" data-src="sovrum städning.jpeg" alt="Thumbnail [100%x225]" src="sovrum städning.jpeg" data-holder-rendered="true"></img>
                   <div className="card-body">
-                    <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <p className="card-text">Sovrum</p>
                     <div className="d-flex justify-content-between align-items-center">
                       <div className="btn-group">
-                        <button type="button" className="btn btn-sm btn-outline-secondary">View</button>
-                        <button type="button" className="btn btn-sm btn-outline-secondary">Edit</button>
+                        <button type="button" className="btn btn-sm btn-outline-secondary">Boka</button>
                       </div>
-                      <small className="text-muted">9 mins</small>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="col-md-4">
-                <div className="card mb-4 box-shadow">
-                  <div className="card-body">
-                    <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                    <div className="d-flex justify-content-between align-items-center">
-                      <div className="btn-group">
-                        <button type="button" className="btn btn-sm btn-outline-secondary">View</button>
-                        <button type="button" className="btn btn-sm btn-outline-secondary">Edit</button>
-                      </div>
-                      <small className="text-muted">9 mins</small>
+                      <small className="text-muted">Ca. 1h</small>
                     </div>
                   </div>
                 </div>
               </div>
               <div className="col-md-4">
                 <div className="card mb-4 box-shadow">
+                <img className="card-img-top" data-src="fönster.jpeg" alt="Thumbnail [100%x225]" src="fönster.jpeg" data-holder-rendered="true"></img>
                   <div className="card-body">
-                    <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <p className="card-text">Fönsterputtsning</p>
                     <div className="d-flex justify-content-between align-items-center">
                       <div className="btn-group">
-                        <button type="button" className="btn btn-sm btn-outline-secondary">View</button>
-                        <button type="button" className="btn btn-sm btn-outline-secondary">Edit</button>
+                        <button type="button" className="btn btn-sm btn-outline-secondary">Boka</button>
                       </div>
-                      <small className="text-muted">9 mins</small>
-                    </div>
-                  </div>
-                </div>
-              </div>
-  
-              <div className="col-md-4">
-                <div className="card mb-4 box-shadow">
-                  <div className="card-body">
-                    <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                    <div className="d-flex justify-content-between align-items-center">
-                      <div className="btn-group">
-                        <button type="button" className="btn btn-sm btn-outline-secondary">View</button>
-                        <button type="button" className="btn btn-sm btn-outline-secondary">Edit</button>
-                      </div>
-                      <small className="text-muted">9 mins</small>
+                      <small className="text-muted">Ca. 1h</small>
                     </div>
                   </div>
                 </div>
               </div>
               <div className="col-md-4">
                 <div className="card mb-4 box-shadow">
+                <img className="card-img-top" data-src="flyttstädning2.jpeg" alt="Thumbnail [100%x225]" src="flyttstädning2.jpeg" data-holder-rendered="true"></img>
                   <div className="card-body">
-                    <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <p className="card-text">Flyttstädning</p>
                     <div className="d-flex justify-content-between align-items-center">
                       <div className="btn-group">
-                        <button type="button" className="btn btn-sm btn-outline-secondary">View</button>
-                        <button type="button" className="btn btn-sm btn-outline-secondary">Edit</button>
+                        <button type="button" className="btn btn-sm btn-outline-secondary">Boka</button>
                       </div>
-                      <small className="text-muted">9 mins</small>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="col-md-4">
-                <div className="card mb-4 box-shadow">
-                  <div className="card-body"><p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                    <div className="d-flex justify-content-between align-items-center">
-                      <div className="btn-group">
-                        <button type="button" className="btn btn-sm btn-outline-secondary">View</button>
-                        <button type="button" className="btn btn-sm btn-outline-secondary">Edit</button>
-                      </div>
-                      <small className="text-muted">9 mins</small>
+                      <small className="text-muted"></small>
                     </div>
                   </div>
                 </div>
@@ -183,16 +123,13 @@ function Bootstrap() {
             </div>
           </div>
         </div>
-  
       </main>
-  
+
       <footer className="text-muted">
         <div className="container">
           <p className="float-right">
             <a href="#">Back to top</a>
           </p>
-          <p>Album example is &copy; Bootstrap, but please download and customize it for yourself!</p>
-          <p>New to Bootstrap? <a href="../../">Visit the homepage</a> or read our <a href="../../getting-started/">getting started guide</a>.</p>
         </div>
       </footer>s
       </>
